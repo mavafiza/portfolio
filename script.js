9353753d6592f13b8ab2685292305e86a297d229
mava <mavafiza@gmail.com>
2024-03-22 05:56:05 +0100

    Creating porfolio repo

---
+++ script.js
@@ -0,0 +1,142 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+// function myMenuFunction() {
+//   let menuBtn = document.getElementById("myNavMenu");
+
+//   if (menuBtn.className === "nav-menu") {
+//     menuBtn.className += " responsive";
+//   } else {
+//     menuBtn.className = "nav-menu";
+//   }
+// }
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+window.onscroll = function () { headerShadow() };
+
+function headerShadow() {
+  const navHeader = document.getElementById("header");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "80px";
+    navHeader.style.lineHeight = "70px";
+
+  } else {
+
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
+
+  }
+}
+
+
+/* ----- TYPING EFFECT ----- */
+let typingEffect = new Typed(".typedText", {
+  strings: ["Agile Mindset", "Fullstack Developer", "Figma Designer", "Scrum Master", "Product Owner", "Ironhacker", "Factoría F5 Alumni", "Copywriter"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000
+})
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card', {})
+sr.reveal('.featured-name', { delay: 100 })
+sr.reveal('.featured-text-info', { delay: 200 })
+sr.reveal('.featured-text-btn', { delay: 200 })
+sr.reveal('.social_icons', { delay: 200 })
+sr.reveal('.featured-image', { delay: 300 })
+
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box', { interval: 200 })
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header', {})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info', { delay: 100 })
+srLeft.reveal('.contact-info', { delay: 100 })
+srLeft.reveal('.certifications', { delay: 100 })
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box', { delay: 100 })
+srRight.reveal('.form-control', { delay: 100 })
+srRight.reveal('.education', { delay: 100 })
+
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+    } else {
+
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+
+
+/* ----- DOWNLOAD PDF MAVAFIZA CV ----- */
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+  // URL del archivo PDF que se va a descargar
+  const pdfUrl = './assets/mavafiza.pdf';
+
+  // Nombre del archivo al descargar
+  const pdfFileName = 'mavafiza.pdf';
+
+  // Crea un elemento <a> temporal
+  const link = document.createElement('a');
+  link.href = pdfUrl;
+  link.download = pdfFileName;
+
+  // Simula un clic en el enlace para iniciar la descarga
+  link.click();
+
+  // Libera el elemento <a> temporal
+  document.body.removeChild(link);
+});
